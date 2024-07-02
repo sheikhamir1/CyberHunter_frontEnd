732b95c69e284bd2aa17efe3b873a9159c44795d
Sheikh amir <Sheikhamir036@gmail.com>
2024-07-03 00:46:31 +0530

    v4

--- src/User_Comp/verify-email_Comp.jsx
+++ src/User_Comp/verify-email_Comp.jsx
@@ -15,7 +15,7 @@ function Verify_email() {
   }, []);
   const fetchMsg = async () => {
     const response = await fetch(
-      `https://cyberhunter-backendend-1.onrender.com/api/user/Verify_email/${token}`
+      `${import.meta.env.VITE_API_URL}/api/user/Verify_email/${token}`
     );
     const data = await response.json();
     console.log(data);
